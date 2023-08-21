e1cda642f044dc2fda2c134391934e6773dd4dbf
Sinkyo <yhko1988@gmail.com>
2023-08-21 17:58:36 +0900

    refactor : options을 상위 컴포넌트로 넘겨서 받도록함
    - 네이밍 변경 및 key 추가

--- src/components/common/ContextMenu.tsx
+++ src/components/common/ContextMenu.tsx
@@ -1,61 +1,23 @@
 import useUnmountIfClickedOutside from "@/hooks/useUnmountIfClickedOutside";
-import { useUserStore } from "@/store/store";
-import React, { Dispatch, SetStateAction, useRef, useState } from "react";
+import React, { Dispatch, SetStateAction, useRef } from "react";
 
 interface IContextMenu {
+  contextMenuOptions: { name: string; callBack: () => void }[];
   position: { x: number; y: number };
   setContextMenuVisible: Dispatch<SetStateAction<boolean>>;
-  setShowPhotoLibrary: Dispatch<SetStateAction<boolean>>;
-  setGrabImage: Dispatch<SetStateAction<boolean>>;
-  setShowCapturePhoto: Dispatch<SetStateAction<boolean>>;
 }
 
 const ContextMenu = ({
+  contextMenuOptions,
   position,
   setContextMenuVisible,
-  setShowPhotoLibrary,
-  setGrabImage,
-  setShowCapturePhoto,
 }: IContextMenu) => {
-  const contextMenuRef = useRef(null);
+  const $contextMenu = useRef(null);
 
   useUnmountIfClickedOutside({
-    ref: contextMenuRef,
+    ref: $contextMenu,
     callback: () => setContextMenuVisible(false),
   });
-
-  const setImage = useUserStore((set) => set.setNewImgSrc);
-
-  const contextMenuOptions = [
-    {
-      name: "Take Photo",
-      callBack: () => {
-        setContextMenuVisible(false);
-        setShowCapturePhoto(true);
-      },
-    },
-    {
-      name: "Choose from Library",
-      callBack: () => {
-        setContextMenuVisible(false);
-        setShowPhotoLibrary(true);
-      },
-    },
-    {
-      name: "Upload Photo",
-      callBack: () => {
-        setContextMenuVisible(false);
-        setGrabImage(true);
-      },
-    },
-    {
-      name: "Remove Photo",
-      callBack: () => {
-        setContextMenuVisible(false);
-        setImage("/default_avatar.png");
-      },
-    },
-  ];
 
   const handleMenuClick =
     (callback: () => void) => (e: React.MouseEvent<HTMLLIElement>) => {
@@ -65,7 +27,7 @@ const ContextMenu = ({
   return (
     <div
       className={`bg-dropdown-background fixed z-[100]`}
-      ref={contextMenuRef}
+      ref={$contextMenu}
       style={{
         boxShadow:
           "0 2px 5px 0 rgba(var(11,20,26),.26),0 2px 10px 0 rgba(11,20,26;),.16)",
@@ -75,14 +37,13 @@ const ContextMenu = ({
     >
       <ul>
         {contextMenuOptions.map(({ name, callBack }) => (
-          <>
-            <li
-              className="hover:bg-background-default-hover px-5 py-2 cursor-pointer"
-              onClick={handleMenuClick(callBack)}
-            >
-              <span className="text-white">{name}</span>
-            </li>
-          </>
+          <li
+            className="hover:bg-background-default-hover px-5 py-2 cursor-pointer"
+            onClick={handleMenuClick(callBack)}
+            key={name}
+          >
+            <span className="text-white">{name}</span>
+          </li>
         ))}
       </ul>
     </div>
