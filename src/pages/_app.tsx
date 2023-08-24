98cdaf159a8a7e276a454e3d8fc397c7617860a7
Sinkyo <yhko1988@gmail.com>
2023-08-24 15:35:30 +0900

    feat : react query 초기세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,29 @@
-import Layout from "@/components/common/Layout";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
+import Layout from "@/components/common/Layout";
+
+import "@/styles/globals.css";
+
+import { QueryClient, QueryClientProvider, Hydrate } from "react-query";
+import { useState } from "react";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <>
       <Head>
         <title>Whatsapp</title>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Hydrate>
+      </QueryClientProvider>
     </>
   );
 }
