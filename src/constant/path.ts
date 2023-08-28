eeb1d1e42ee0f84029cc1f0539f24d2e3f72ec9f
Sinkyo <yhko1988@gmail.com>
2023-08-28 17:08:05 +0900

    fix : 백엔드랑 url 맞춰야함ㅋㅋ

--- src/constant/path.ts
+++ src/constant/path.ts
@@ -1,5 +1,5 @@
 const AUTH_ROUTE = "/api/auth";
 
-export const SIGN_IN_USER = `${AUTH_ROUTE}/signIn`;
+export const SIGN_IN_USER = `${AUTH_ROUTE}/sign-in`;
 
-export const GO_TO_SIGN_UP = `${AUTH_ROUTE}/signUp`;
+export const GO_TO_SIGN_UP = `${AUTH_ROUTE}/sign-up`;
