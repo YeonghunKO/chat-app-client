175a4f2291e6ce45d434bdf5aac3ad984c5d1545
Sinkyo <yhko1988@gmail.com>
2023-08-28 16:56:11 +0900

    refactor : 깔끔하게 두개로 분류

--- src/constant/path.ts
+++ src/constant/path.ts
@@ -1,3 +1,5 @@
 const AUTH_ROUTE = "/api/auth";
 
-export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`;
+export const SIGN_IN_USER = `${AUTH_ROUTE}/signIn`;
+
+export const GO_TO_SIGN_UP = `${AUTH_ROUTE}/signUp`;
