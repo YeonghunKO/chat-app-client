2d75ac4945d4e40bc5f4133a5c852b176161a10f
Sinkyo <yhko1988@gmail.com>
2023-08-29 16:12:40 +0900

    feat : 로그인 페이지 완성

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,80 +1,113 @@
+//setting
+import { useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
-import { CHECK_USER_ROUTE } from "@/constant/path";
-
+//business
 import { firebaseAuth } from "@/utils/firebaseConfig";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { SIGN_IN_USER } from "@/constant/api";
+import { DASHBOARD, SIGN_UP_PAGE } from "@/constant/path";
+import { queryKeys } from "@/constant/queryKeys";
+import { usePostMutationQueryAccount } from "@/hooks/useUserQueryAccount";
 
+//compoenents
+import Input from "@/components/common/Input";
 import { FcGoogle } from "react-icons/fc";
+import { useUiState } from "@/store/store";
 
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useUserStore } from "@/store/store";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-
-export default function Login({
-  repo,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log("repo", repo);
-
+export default function Login() {
   const router = useRouter();
 
-  const handleLogin = async () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const updateToast = useUiState((state) => state.updateToastInfo);
+
+  const { mutate } = usePostMutationQueryAccount({
+    queryKey: queryKeys.userInfo,
+    url: SIGN_IN_USER,
+    onSuccess: () => router.push(DASHBOARD),
+    onError: (err: any) => {
+      updateToast({
+        type: "ERROR",
+        msg: err.response.data.message,
+      });
+    },
+  });
+
+  const handleAccountLogin = async () => {
+    if (!email || !password) {
+      updateToast({
+        type: "ERROR",
+        msg: "email and password are required",
+      });
+      return;
+    }
+    mutate({
+      email,
+      password,
+    });
+  };
+
+  const handleGoogleLogin = async () => {
     const provider = new GoogleAuthProvider();
     const {
-      user: { displayName: name, email, photoURL: profileImage },
+      user: { email, displayName, photoURL },
     } = await signInWithPopup(firebaseAuth, provider);
-    try {
-      const { data } = await axios.post(CHECK_USER_ROUTE, { email });
-      console.log("res", data.status);
+    mutate({
+      email,
+      loginType: "google",
+    });
+  };
 
-      if (!data.status) {
-        router.push("/signUp ");
-      } else {
-        router.push("/");
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  const handleCreateAccount = () => {
+    router.push(SIGN_UP_PAGE);
   };
 
   return (
-    <div className="flex justify-center items-center bg-panel-header-background h-screen w-screen flex-col gap-6">
-      <div className="flex items-center justify-center gap-2 text-white">
-        <Image
-          src="/whatsapp.gif"
-          alt="whatsapp-gif"
-          height={300}
-          width={300}
-        />
-        <span className="text-7xl">WhatsApp</span>
+    <div className="flex h-screen w-screen flex-col items-center justify-center bg-panel-header-background">
+      <div className="flex h-4/5 w-screen items-center justify-center gap-6 bg-panel-header-background">
+        <div className="flex flex-col items-center justify-center gap-2 text-white">
+          <Image
+            src="/whatsapp.gif"
+            alt="whatsapp-gif"
+            height={300}
+            width={300}
+          />
+          <span className="text-7xl">WhatsApp</span>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-4 text-white">
+          <Input label="Display Email" value={email} setValue={setEmail} />
+          <Input
+            label="Password"
+            value={password}
+            setValue={setPassword}
+            type="password"
+          />
+
+          <button
+            className="flex w-full items-center justify-center gap-7 rounded-lg bg-search-input-container-background p-5"
+            onClick={handleAccountLogin}
+          >
+            <span className="text-2xl text-white">Login an Account</span>
+          </button>
+          <button
+            className="flex w-full items-center justify-center gap-7 rounded-lg bg-search-input-container-background p-5"
+            onClick={handleGoogleLogin}
+          >
+            <FcGoogle className="text-4xl" />
+            <span className="text-2xl text-white">Login With Google</span>
+          </button>
+        </div>
       </div>
-      <button
-        className="flex items-center justify-center gap-7 bg-search-input-container-background p-5 rounded-lg"
-        onClick={handleLogin}
+
+      <span
+        className="cursor-pointer text-teal-light"
+        onClick={handleCreateAccount}
       >
-        <FcGoogle className="text-4xl" />
-        <span className="text-white text-2xl">Login With Google</span>
-      </button>
+        create an account
+      </span>
     </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps<{ repo: string }> = async (
-  context
-) => {
-  // if (!data) {
-  //   return {
-  //     redirect: {
-  //       destination: '/',
-  //       permanent: false,
-  //     },
-  //   }
-  // }
-  //
-  return {
-    props: {
-      repo: "cvz",
-    },
-  };
-};
