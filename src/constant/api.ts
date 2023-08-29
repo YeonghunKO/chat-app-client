5f63cd8a9aad132740ac323e18049aaeec5f5e76
Sinkyo <yhko1988@gmail.com>
2023-08-29 16:09:53 +0900

    refactor : 상수 분리

---
+++ src/constant/api.ts
@@ -0,0 +1,5 @@
+const AUTH_ROUTE = "/api/auth";
+
+export const SIGN_IN_USER = `${AUTH_ROUTE}/sign-in`;
+export const SIGN_UP_USER = `${AUTH_ROUTE}/sign-up`;
+export const REFRESH = `${AUTH_ROUTE}/refresh`;
--- src/constant/path.ts
+++ src/constant/path.ts
@@ -1,5 +1,2 @@
-const AUTH_ROUTE = "/api/auth";
-
-export const SIGN_IN_USER = `${AUTH_ROUTE}/sign-in`;
-
-export const GO_TO_SIGN_UP = `${AUTH_ROUTE}/sign-up`;
+export const SIGN_UP_PAGE = "/signUp";
+export const DASHBOARD = "/";
