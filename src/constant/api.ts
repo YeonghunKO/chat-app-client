520d71ef0664ea338cda92d0f033697a56cd03ed
Sinkyo <yhko1988@gmail.com>
2023-08-31 14:05:35 +0900

    feat : dashboard('/')에 도착하면 무조건 토큰 검증부터 시작

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -3,3 +3,8 @@ const AUTH_ROUTE = "/api/auth";
 export const SIGN_IN_USER = `${AUTH_ROUTE}/sign-in`;
 export const SIGN_UP_USER = `${AUTH_ROUTE}/sign-up`;
 export const REFRESH = `${AUTH_ROUTE}/refresh`;
+
+export const COOKIE = {
+  REFRESH_IDX: "refreshTokenIdx",
+  ACCESS_TOKEN: "accessToken",
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,84 @@
+import { COOKIE, REFRESH } from "@/constant/api";
+import { SIGN_IN_PAGE } from "@/constant/path";
+import { queryKeys } from "@/constant/queryKeys";
+import { postFetch } from "@/lib/api";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { QueryClient, dehydrate, useQueryClient } from "react-query";
+
+import Cookies from "cookies";
+
 export default function Home() {
-  return <main></main>;
+  const router = useRouter();
+  const client = useQueryClient();
+  console.log("client", client.getQueryData(queryKeys.userInfo));
+
+  return (
+    <main>
+      <button onClick={() => router.push("/login")}>go to login</button>
+    </main>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps<{}> = async ({
+  req,
+  res,
+}) => {
+  const cookies = new Cookies(req, res);
+
+  const queryClient = new QueryClient();
+
+  const { accessToken, refreshTokenIdx } = req.cookies;
+  if (!accessToken || !refreshTokenIdx) {
+    return {
+      redirect: {
+        destination: SIGN_IN_PAGE,
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    const result = await queryClient.fetchQuery(queryKeys.userInfo, () =>
+      postFetch({
+        url: REFRESH,
+        option: {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            refreshTokenIdx: decodeURIComponent(refreshTokenIdx),
+          },
+        },
+        mapper: (data) => ({
+          name: data.user.name,
+          email: data.user.email,
+          about: data.user.about,
+          profilePicture: data.user.profilePicture,
+        }),
+      }),
+    );
+
+    const newAccessToken = result.accessToken;
+    newAccessToken && cookies.set(COOKIE.ACCESS_TOKEN, newAccessToken);
+
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+      },
+    };
+  } catch (error: any) {
+    const redirctDestination = error.response.data.redirctDestination;
+
+    if (redirctDestination) {
+      return {
+        redirect: {
+          destination: SIGN_IN_PAGE,
+          permanent: false,
+        },
+      };
+    }
+  }
+
+  return {
+    props: {},
+  };
+};
