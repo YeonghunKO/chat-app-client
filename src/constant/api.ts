868bed7d05ae192bfb24710352f22d8c8d69d9e7
Sinkyo <yhko1988@gmail.com>
2023-09-04 13:56:47 +0900

    feat : 모든 users가져오기

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -3,6 +3,8 @@ const AUTH_ROUTE = "/api/auth";
 export const SIGN_IN_USER = `${AUTH_ROUTE}/sign-in`;
 export const SIGN_UP_USER = `${AUTH_ROUTE}/sign-up`;
 export const REFRESH = `${AUTH_ROUTE}/refresh`;
+export const GET_USER = (email: string) => `${AUTH_ROUTE}/user/${email}`;
+export const GET_ALL_USERS = `${AUTH_ROUTE}/users`;
 
 export const COOKIE = {
   REFRESH_IDX: "refreshTokenIdx",
--- src/constant/queryKeys.ts
+++ src/constant/queryKeys.ts
@@ -1,3 +1,4 @@
 export const queryKeys = {
   userInfo: ["userInfo"],
+  users: ["users"],
 };
