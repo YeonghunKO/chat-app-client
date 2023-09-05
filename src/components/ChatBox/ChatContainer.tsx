4a2389e29e3444c811e7113bef28047339c8e74a
Sinkyo <yhko1988@gmail.com>
2023-09-05 15:06:24 +0900

    feat : chat box에 필요한 컴포넌트 ui만 대략적으로 완성

--- src/components/ChatBox/ChatContainer.tsx
+++ src/components/ChatBox/ChatContainer.tsx
@@ -1,5 +1,57 @@
+import { useRef } from "react";
+
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const containerRef = useRef(null);
+  return (
+    <div
+      className="custom-scrollbar relative h-[80vh] w-full flex-grow overflow-auto "
+      ref={containerRef}
+    >
+      <div className="fixed left-0 top-0 z-0 h-full w-full bg-chat-background bg-fixed opacity-5"></div>
+      <div className="relative bottom-0 left-0 z-40 mx-10 my-6 ">
+        <div className="flex w-full">
+          <div className="flex w-full flex-col justify-end gap-1 overflow-auto">
+            {/* {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`flex ${
+              message.senderId === currentChatUser.id
+                ? "justify-start"
+                : "justify-end"
+            }`}
+          >
+            {message.type === "text" && (
+              <div
+                className={`text-white px-2 py-[5px] text-sm rounded-md flex gap-2 items-end max-w-[45%]	 ${
+                  message.senderId === currentChatUser.id
+                    ? "bg-incoming-background"
+                    : "bg-outgoing-background"
+                }`}
+              >
+                <span className="break-all">{message.message}</span>
+                <div className="flex gap-1 items-end">
+                  <span className="text-bubble-meta text-[11px] pt-1 min-w-fit">
+                    {calculateTime(message.createdAt)}
+                  </span>
+                  <span>
+                    {message.senderId === userInfo.id && (
+                      <MessageStatus
+                        messageStatus={message.messageStatus}
+                      />
+                    )}
+                  </span>
+                </div>
+              </div>
+            )}
+            {message.type === "image" && <ImageMessage message={message} />}
+            {message.type === "audio" && <VoiceMessage message={message} />}
+          </div>
+        ))} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatContainer;
--- src/components/ChatBox/MessageBar.tsx
+++ src/components/ChatBox/MessageBar.tsx
@@ -1,5 +1,65 @@
+import { BsEmojiSmile } from "react-icons/bs";
+import { ImAttachment } from "react-icons/im";
+import { FaMicrophone } from "react-icons/fa";
+import { MdSend } from "react-icons/md";
+
 const MessageBar = () => {
-  return <div>MessageBar</div>;
+  return (
+    <div className="relative flex h-20 items-center gap-6 bg-panel-header-background  px-4">
+      <>
+        <div className="flex gap-6">
+          <BsEmojiSmile
+            className="cursor-pointer text-xl text-panel-header-icon"
+            title="Emoji"
+            //   onClick={handleEmojiModal}
+            id="emoji-open"
+          />
+          {/* {showEmojiPicker && (
+          <div
+            className="absolute bottom-24 left-16 z-40"
+            ref={emojiPickerRef}
+          >
+            <EmojiPicker onEmojiClick={handleEmojiClick} theme="dark" />
+          </div>
+        )} */}
+          <ImAttachment
+            className="cursor-pointer text-xl text-panel-header-icon"
+            title="Attach"
+            //   onClick={() => setGrabImage(true)}
+          />
+        </div>
+        <div className="flex h-10 w-full items-center rounded-lg">
+          <input
+            type="text"
+            placeholder="Type a message"
+            className="h-10 w-full rounded-lg bg-input-background py-4 pl-5 pr-5 text-sm text-white focus:outline-none"
+            //   value={message}
+            //   onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <div className=" flex w-10 items-center justify-center gap-2">
+          <FaMicrophone
+            className="cursor-pointer text-xl text-panel-header-icon"
+            title="Record"
+            // onClick={() => setShowAudioRecorder(true)}
+          />
+          <MdSend
+            className="cursor-pointer text-xl text-panel-header-icon"
+            title="Send"
+          />
+          {/* {message.length ? (
+          <button
+        //    onClick={sendMessage}
+          >
+          </button>
+        ) : (
+        )} */}
+        </div>
+      </>
+      {/* {showAudioRecorder && <CaptureAudio hide={setShowAudioRecorder} />}
+  {grabImage && <PhotoPicker onChange={photoPickerOnChange} />}  */}
+    </div>
+  );
 };
 
 export default MessageBar;
