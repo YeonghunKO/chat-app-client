d27b8ab737808826ba0e09b46bb18ee4b9a31c52
Sinkyo <yhko1988@gmail.com>
2023-09-05 10:51:13 +0900

    feat : chat list 선언

--- src/components/ChatList/ChatList.tsx
+++ src/components/ChatList/ChatList.tsx
@@ -1,5 +1,27 @@
+import { useUiState } from "@/store";
+import ChatListHeader from "./ChatListHeader";
+import List from "./List";
+import SearchBar from "./SearchBar";
+import ContactsList from "./ContactsList";
+
 const ChatList = () => {
-  return <div>ChatList</div>;
+  const isContactsListVisible = useUiState((set) => set.isContactsVisible);
+
+  return (
+    <section className="z-20 flex max-h-screen flex-col bg-panel-header-background">
+      {isContactsListVisible ? (
+        <>
+          <ContactsList />
+        </>
+      ) : (
+        <>
+          <ChatListHeader />
+          <SearchBar />
+          <List />
+        </>
+      )}
+    </section>
+  );
 };
 
 export default ChatList;
