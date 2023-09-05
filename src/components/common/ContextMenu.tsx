20cfd3845b1d5994b1b21ea6e4e3acec9d4084c8
Sinkyo <yhko1988@gmail.com>
2023-09-05 14:49:16 +0900

    fix : 메뉴가 body바깥으로 삐져나오지 않게 계산

--- src/components/common/ContextMenu.tsx
+++ src/components/common/ContextMenu.tsx
@@ -1,5 +1,12 @@
 import useUnmountIfClickedOutside from "@/hooks/useUnmountIfClickedOutside";
-import React, { Dispatch, SetStateAction, useRef } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 
 interface IContextMenu {
   contextMenuOptions: { name: string; callBack: () => void }[];
@@ -12,7 +19,8 @@ const ContextMenu = ({
   position,
   setContextMenuVisible,
 }: IContextMenu) => {
-  const $contextMenu = useRef(null);
+  const $contextMenu = useRef<HTMLDivElement>(null);
+  const [suitableXPos, setSuitableXPos] = useState(0);
 
   useUnmountIfClickedOutside({
     ref: $contextMenu,
@@ -24,21 +32,40 @@ const ContextMenu = ({
       callback();
     };
 
+  // prevent menu from sticking to the outside of body
+  useEffect(() => {
+    const getSuitableXPos = () => {
+      let isMenuStickingOutside;
+      if ($contextMenu.current) {
+        const bodyWidth = document.body.getBoundingClientRect().width;
+        const menuWidth = $contextMenu.current.getBoundingClientRect().width;
+        isMenuStickingOutside = bodyWidth < position.x + menuWidth;
+        if (isMenuStickingOutside) {
+          setSuitableXPos(bodyWidth - menuWidth - 10);
+        } else {
+          setSuitableXPos(position.x);
+        }
+      }
+    };
+
+    getSuitableXPos();
+  }, [$contextMenu.current]);
+
   return (
     <div
-      className={`bg-dropdown-background fixed z-[100]`}
+      className="fixed z-[100] bg-dropdown-background"
       ref={$contextMenu}
       style={{
         boxShadow:
           "0 2px 5px 0 rgba(var(11,20,26),.26),0 2px 10px 0 rgba(11,20,26;),.16)",
         top: position.y,
-        left: position.x,
+        left: suitableXPos,
       }}
     >
       <ul>
         {contextMenuOptions.map(({ name, callBack }) => (
           <li
-            className="hover:bg-background-default-hover px-5 py-2 cursor-pointer"
+            className="cursor-pointer px-5 py-2 hover:bg-background-default-hover"
             onClick={handleMenuClick(callBack)}
             key={name}
           >
