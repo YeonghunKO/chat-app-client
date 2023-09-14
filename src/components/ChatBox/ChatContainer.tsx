3960ba208424a9804acc3e0d15cc8c24468167ef
Sinkyo <yhko1988@gmail.com>
2023-09-14 16:02:22 +0900

    feat : 스크롤 항상 가장 밑으로 내려옴

--- src/components/ChatBox/ChatContainer.tsx
+++ src/components/ChatBox/ChatContainer.tsx
@@ -1,30 +1,22 @@
 import { useGetCurrentMessagesQuery } from "@/hooks/useQueryAccount";
 import { IMessage } from "@/type";
-import { useEffect, useRef } from "react";
 import TextMessage from "./TextMessage";
 
 const ChatContainer = () => {
-  const $messageEnd = useRef<HTMLDivElement>(null);
-
   const { data: messages } = useGetCurrentMessagesQuery<IMessage[]>({});
 
   console.log("messages", messages);
 
-  useEffect(() => {
-    // $messageEnd.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
-
   return (
-    <div className="custom-scrollbar relative h-[80vh] w-full flex-grow overflow-auto ">
+    <div className="custom-scrollbar flex h-[80dvh] w-full flex-grow flex-col-reverse overflow-auto ">
       <div className="fixed left-0 top-0 z-0 h-full w-full bg-chat-background bg-fixed opacity-5"></div>
-      <div className="relative bottom-0 left-0 z-40 mx-10 my-6 ">
-        <section className="flex w-full">
-          <div className="flex w-full flex-col justify-end gap-1 overflow-auto">
+      <div className="relative mx-10 my-6 ">
+        <section className="w-full">
+          <div className="flex w-full flex-col gap-1 overflow-auto">
             {messages?.map((message) => {
               return <TextMessage message={message} key={message.id} />;
             })}
           </div>
-          <div ref={$messageEnd}></div>
         </section>
       </div>
     </div>
