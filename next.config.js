6b68e1f2b5521147998d0110c60e06c162555941
Sinkyo <yhko1988@gmail.com>
2023-09-19 14:13:55 +0900

    fix : image 허용 domain 설정

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    domains: ["localhost"],
     remotePatterns: [
       {
-        protocol: "https",
         hostname: "**",
+        pathname: "/uploads/images/**",
       },
     ],
   },
