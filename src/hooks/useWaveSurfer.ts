bb1471905ad70fb673c25f81b72c47945e302d4f
Sinkyo <yhko1988@gmail.com>
2023-09-22 13:05:02 +0900

    feat : waveSurfer init

--- src/hooks/useWaveSurfer.ts
+++ src/hooks/useWaveSurfer.ts
@@ -1,1 +1,33 @@
-export const useWaveSurfer = () => {};
+import { RefObject, useEffect, useState } from "react";
+import WaveSurfer from "wavesurfer.js";
+
+interface IUseWaveSurfer {
+  container: RefObject<HTMLDivElement>;
+}
+
+export const useWaveSurfer = ({ container }: IUseWaveSurfer) => {
+  const [waveform, setWaveform] = useState<WaveSurfer>();
+
+  // init waveSurfer
+  useEffect(() => {
+    if (container.current) {
+      const waveSurfer = WaveSurfer.create({
+        container: container.current as HTMLDivElement,
+        waveColor: "#ccc",
+        progressColor: "#4a9eff",
+        cursorColor: "#7ae3c3",
+        barWidth: 2,
+        height: 30,
+        responsive: true,
+      });
+
+      setWaveform(waveSurfer);
+
+      return () => {
+        waveSurfer.destroy();
+      };
+    }
+  }, []);
+
+  return [waveform];
+};
