584c2cb0923a5054e6e538109bffd5511d99b3ba
Sinkyo <yhko1988@gmail.com>
2023-10-02 16:19:37 +0900

    feat : 메시지 검색 ui완성

--- src/components/ChatBox/SearchMessages.tsx
+++ src/components/ChatBox/SearchMessages.tsx
@@ -1,8 +1,78 @@
+import { useGetCurrentMessagesQuery } from "@/hooks/useQueryAccount";
+import { useSearchStore, useUserStore } from "@/store";
+import { useEffect, useState } from "react";
+import { IoClose } from "react-icons/io5";
+import Input from "../common/Input";
+import { IMessage } from "@/type";
+import { calculateTime } from "@/utils/calculateTime";
+
 const SearchMessages = () => {
+  const toggleSearching = useSearchStore((set) => set.toggleIsSearchingMessage);
+
+  const handleCloseSearching = () => {
+    toggleSearching();
+  };
+
+  const currentChatUser = useUserStore((set) => set.currentChatUser);
+
+  const [searchedMessages, setSearchedMessages] = useState<IMessage[]>([]);
+  const [searchingKeyWord, setSearchingKeyWord] = useState("");
+
+  const { data: allMessages } = useGetCurrentMessagesQuery<IMessage[]>();
+
+  useEffect(() => {
+    if (allMessages) {
+      if (searchingKeyWord) {
+        const filteredMessages = allMessages.filter(
+          (message) =>
+            message.type === "text" &&
+            message.message.includes(searchingKeyWord),
+        );
+        setSearchedMessages(filteredMessages);
+      } else {
+        setSearchedMessages([]);
+      }
+    }
+  }, [searchingKeyWord, allMessages]);
+
   return (
-    <div id="search_messages" className="w-full bg-slate-600">
-      SearchMessages
-    </div>
+    <section className="z-10 w-full  bg-search-input-container-background">
+      <header className="flex h-[57px] gap-[10px] bg-panel-header-background pl-[10px] pt-[18px]">
+        <IoClose
+          className="cursor-pointer text-2xl text-icon-lighter"
+          onClick={handleCloseSearching}
+        />
+        <p>Search Messages</p>
+      </header>
+      <div className="p-[10px]">
+        <Input
+          value={searchingKeyWord}
+          setValue={setSearchingKeyWord}
+          placeholder="searching messages"
+        />
+      </div>
+      <div className="custom-scrollbar h-[80dvh] overflow-auto p-[20px] pt-[0]">
+        {searchedMessages.length ? (
+          searchedMessages.map((message: IMessage) => {
+            return (
+              <div
+                key={message.id}
+                className="mb-[10px] cursor-pointer rounded-md p-[10px] hover:bg-incoming-background hover:text-teal-light"
+              >
+                <p className="text-panel-header-icon">
+                  {calculateTime(message.createdAt)}
+                </p>
+                <p>{message.message}</p>
+              </div>
+            );
+          })
+        ) : (
+          <p className="mt-[40px] text-center text-panel-header-icon">
+            Search Messages with {currentChatUser?.name}
+          </p>
+        )}
+      </div>
+    </section>
   );
 };
 
