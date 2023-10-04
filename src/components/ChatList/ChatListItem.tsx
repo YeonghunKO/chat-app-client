9bf5d83fb91b4e9abb700eb46ba222f3809fec67
Sinkyo <yhko1988@gmail.com>
2023-10-04 15:40:26 +0900

    refactor : contact클릭시 읽음 표시에 관한 socket을 따로 다시 만듬

--- src/components/ChatList/ChatListItem.tsx
+++ src/components/ChatList/ChatListItem.tsx
@@ -21,9 +21,8 @@ const ChatListItem = ({ userInfo }: { userInfo: IUserInfo }) => {
         userId: loggedInUser.id,
         value: { chatRoomId: id },
       });
-
       // 상태편의 메시지에 내한테 보낸 메시지가 read로 표시되도록 함
-      socket.emit("send-msg", {
+      socket.emit("mark-read", {
         from: loggedInUser.id,
         to: id,
       });
