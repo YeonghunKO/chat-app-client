0d566170e375d8b429c1f5b841d935bcf3087381
Sinkyo <yhko1988@gmail.com>
2023-10-04 15:39:29 +0900

    feat : chatList 데이터 잘들어오는지 테스트 중

--- src/components/ChatList/List.tsx
+++ src/components/ChatList/List.tsx
@@ -1,4 +1,18 @@
+import { UPDATE_CHAT_LIST } from "@/constant/api";
+import { queryKeys } from "@/constant/queryKeys";
+import { useGetQueryAccount } from "@/hooks/useQueryAccount";
+import { useQueryClient } from "react-query";
+
 const List = () => {
+  const queryClient = useQueryClient();
+  const loggedInUser = queryClient.getQueryData(queryKeys.userInfo) as any;
+  const { data } = useGetQueryAccount({
+    queryKey: queryKeys.chatLists,
+    url: UPDATE_CHAT_LIST(loggedInUser?.id),
+  });
+
+  console.log("chatlist data", data);
+
   return <div>List</div>;
 };
 
