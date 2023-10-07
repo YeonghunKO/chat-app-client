a3b6add004372b6f2760ca3f34b28d9942695bd9
Sinkyo <yhko1988@gmail.com>
2023-10-07 22:20:43 +0900

    style : search bar input 조정

--- src/components/ContactInfo/SearchBar.tsx
+++ src/components/ContactInfo/SearchBar.tsx
@@ -1,17 +1,23 @@
 import { BiFilter, BiSearchAlt2 } from "react-icons/bi";
+import Input from "../common/Input";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [value, setValue] = useState("");
   return (
-    <div className="flex h-14 items-center gap-3 bg-search-input-container-background py-3 pl-5">
-      <div className="flex flex-grow items-center gap-5 rounded-lg bg-panel-header-background px-3 py-1">
+    <div className="flex h-14 items-center gap-3 bg-search-input-container-background py-3 pl-3">
+      <div className="flex items-center gap-5 rounded-lg py-1">
         <div>
           <BiSearchAlt2 className="text-l cursor-pointer text-panel-header-icon" />
         </div>
-        <div className="">
-          <input
-            type="text"
-            placeholder="Search or start new chat"
-            className="w-full bg-transparent text-sm text-white focus:outline-none"
+        <div className="flex-grow-[2]">
+          {/* type="text"
+          placeholder="Search or start new chat"
+          className="w-full bg-transparent text-sm text-white focus:outline-none" */}
+          <Input
+            value={value}
+            setValue={setValue}
+            placeholder="search new chat"
           />
         </div>
       </div>
