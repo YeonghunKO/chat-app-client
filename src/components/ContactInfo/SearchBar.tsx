8ac885339247c69f15fc32d061247d4f4c89cb26
Sinkyo <yhko1988@gmail.com>
2023-10-09 13:33:46 +0900

    feat : chat list 검색 너비 맞춤

--- src/components/ContactInfo/SearchBar.tsx
+++ src/components/ContactInfo/SearchBar.tsx
@@ -6,14 +6,11 @@ const SearchBar = () => {
   const [value, setValue] = useState("");
   return (
     <div className="flex h-14 items-center gap-3 bg-search-input-container-background py-3 pl-3">
-      <div className="flex items-center gap-5 rounded-lg py-1">
+      <div className="flex w-full items-center gap-5 rounded-lg py-1">
         <div>
           <BiSearchAlt2 className="text-l cursor-pointer text-panel-header-icon" />
         </div>
         <div className="flex-grow-[2]">
-          {/* type="text"
-          placeholder="Search or start new chat"
-          className="w-full bg-transparent text-sm text-white focus:outline-none" */}
           <Input
             value={value}
             setValue={setValue}
