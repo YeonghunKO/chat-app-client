2360805d3243c2ec574441b88e202af97d1b7531
Sinkyo <yhko1988@gmail.com>
2023-10-10 17:02:25 +0900

    feat : 모든 메시지 찾기 기능 구현 완료

--- src/components/ContactInfo/ChatList.tsx
+++ src/components/ContactInfo/ChatList.tsx
@@ -1,13 +1,24 @@
-import { UPDATE_CHAT_LIST } from "@/constant/api";
+import { FILTERED_MESSAGE, UPDATE_CHAT_LIST } from "@/constant/api";
 import { queryKeys } from "@/constant/queryKeys";
 import { useGetQueryAccount } from "@/hooks/useQueryAccount";
 import { useQueryClient } from "react-query";
 import ContactItem from "./ContactItem";
 import { IMessage, IUserInfo } from "@/type";
+import { useSearchStore } from "@/store";
+import { useEffect, useState } from "react";
 
 const ChatList = () => {
   const queryClient = useQueryClient();
+
   const loggedInUser = queryClient.getQueryData(queryKeys.userInfo) as any;
+
+  const { filteredChatListNumbers, searchingValue } = useSearchStore((set) => ({
+    filteredChatListNumbers: set.filteredChatList,
+    searchingValue: set.allMessageSearchValue,
+  }));
+
+  const [filteredChatList, setFilteredChatList] = useState<any[]>();
+
   const {
     data: chatList,
     isLoading,
@@ -22,7 +33,23 @@ const ChatList = () => {
     url: UPDATE_CHAT_LIST(loggedInUser?.id),
   });
 
-  // console.log("chatlist data", chatList[0]);
+  useEffect(() => {
+    if (chatList?.length) {
+      const filteredChatList = chatList?.filter((list) => {
+        const isChatToBeIncluded = filteredChatListNumbers.includes(
+          list.chatUser.id,
+        );
+        if (isChatToBeIncluded) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      setFilteredChatList(filteredChatList);
+    }
+  }, [filteredChatListNumbers, chatList]);
+
   if (isLoading) {
     return (
       <div className=" flex h-full items-center justify-center">loading...</div>
@@ -31,18 +58,38 @@ const ChatList = () => {
 
   if (isSuccess) {
     return (
-      <div className="custom-scrollbar overflow-auto">
-        {chatList.map((list) => {
-          const { chatUser, ...messageInfo } = list;
-          return (
-            <ContactItem
-              key={messageInfo.id}
-              userInfo={chatUser}
-              messageInfo={messageInfo}
-              isChatting={true}
-            />
-          );
-        })}
+      <div className="custom-scrollbar h-full overflow-auto">
+        {searchingValue.length > 0 ? (
+          filteredChatList && filteredChatList?.length > 0 ? (
+            filteredChatList.map((list) => {
+              const { chatUser, ...messageInfo } = list;
+              return (
+                <ContactItem
+                  key={messageInfo.id}
+                  userInfo={chatUser}
+                  messageInfo={messageInfo}
+                  isChatting={true}
+                />
+              );
+            })
+          ) : (
+            <div className="flex h-full items-center justify-center">
+              No Messages Found
+            </div>
+          )
+        ) : (
+          chatList.map((list) => {
+            const { chatUser, ...messageInfo } = list;
+            return (
+              <ContactItem
+                key={messageInfo.id}
+                userInfo={chatUser}
+                messageInfo={messageInfo}
+                isChatting={true}
+              />
+            );
+          })
+        )}
       </div>
     );
   }
--- src/components/ContactInfo/SearchBar.tsx
+++ src/components/ContactInfo/SearchBar.tsx
@@ -1,9 +1,56 @@
+import { useState } from "react";
+import { useQueryClient } from "react-query";
+import { IMessage, IUserInfo } from "@/type";
+
+import { useMutationQuery } from "@/hooks/useQueryAccount";
 import { BiFilter, BiSearchAlt2 } from "react-icons/bi";
 import Input from "../common/Input";
-import { useState } from "react";
+
+import { getFetch } from "@/lib/api";
+import { useDebounce } from "@/hooks/useDebounce";
+import { queryKeys } from "@/constant/queryKeys";
+import { FILTERED_MESSAGE } from "@/constant/api";
+import { useSearchStore } from "@/store";
 
 const SearchBar = () => {
   const [value, setValue] = useState("");
+  const client = useQueryClient();
+
+  const { id } = client.getQueryData(queryKeys.userInfo) as IUserInfo;
+
+  const { setFilteredChatList, setSearchingValue } = useSearchStore((set) => ({
+    setFilteredChatList: set.setFilteredChatList,
+    setSearchingValue: set.setAllMessageSearchValue,
+  }));
+
+  const { mutate, data } = useMutationQuery({
+    queryKey: queryKeys.filteredMessages,
+    mutationFunc: (message: string) =>
+      getFetch({
+        url: FILTERED_MESSAGE(id, message),
+        mapper: (filteredMessages: IMessage[]) => {
+          const parsedChatListId = filteredMessages.reduce((acc, message) => {
+            const otherUserId =
+              message.senderId === id ? message.recieverId : message.senderId;
+            acc.add(otherUserId);
+            return acc;
+          }, new Set());
+
+          return Array.from(parsedChatListId);
+        },
+      }),
+    onSuccess: (data: number[]) => {
+      setFilteredChatList(data);
+    },
+  });
+
+  useDebounce(() => {
+    if (value.length) {
+      mutate(value);
+    }
+    setSearchingValue(value);
+  }, value);
+
   return (
     <div className="flex h-14 items-center gap-3 bg-search-input-container-background py-3 pl-3">
       <div className="flex w-full items-center gap-5 rounded-lg py-1">
