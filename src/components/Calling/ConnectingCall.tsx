482a207752663604100d2d059517492325275bd4
Sinkyo <yhko1988@gmail.com>
2023-10-19 15:42:23 +0900

    feat : 전화거는중일때 ui 완성

--- src/components/Calling/ConnectingCall.tsx
+++ src/components/Calling/ConnectingCall.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SocketCotext } from "../common/CallingContext";
+import Image from "next/image";
+import { MdOutlineCallEnd } from "react-icons/md";
 
 const ConnectingCall = () => {
-  return <div>ConnectingCall</div>;
+  const { callAccepted, call, setCall } = useContext(SocketCotext);
+
+  const endCall = () => {};
+  console.log(call);
+  return (
+    <div className="absolute left-1/2 top-1/2 z-[99] flex h-[80dvh] w-[80dvw] -translate-x-1/2 -translate-y-1/2 items-center justify-center border-2 border-outgoing-background  bg-conversation-panel-background">
+      <div className="flex flex-col items-center gap-[30px]">
+        <div className="text-center">
+          <h1 className="text-[50px]">Harry Potter</h1>
+          <h2>calling</h2>
+        </div>
+        <div className="h-[70%] w-[70%]">
+          <Image src="/avatars/1.png" alt="harry" fill className="!relative" />
+        </div>
+        <button
+          className="flex h-16 w-16 items-center justify-center rounded-full bg-red-600"
+          onClick={endCall}
+        >
+          <MdOutlineCallEnd className="text-3xl" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ConnectingCall;
