b0d70714f5608a299378a6c5f5e64cf349fbae81
Sinkyo <yhko1988@gmail.com>
2023-10-20 17:03:44 +0900

    feat : 수신자의 정보를 표시

--- src/components/Calling/ConnectingCall.tsx
+++ src/components/Calling/ConnectingCall.tsx
@@ -4,19 +4,25 @@ import Image from "next/image";
 import { MdOutlineCallEnd } from "react-icons/md";
 
 const ConnectingCall = () => {
-  const { callAccepted, call, cancelUser, rejectUser } =
-    useContext(SocketCotext);
+  const { call, cancelUser } = useContext(SocketCotext);
 
   console.log(call);
   return (
     <div className="absolute left-1/2 top-1/2 z-[99] flex h-[80dvh] w-[80dvw] -translate-x-1/2 -translate-y-1/2 items-center justify-center border-2 border-outgoing-background  bg-conversation-panel-background">
-      <div className="flex flex-col items-center gap-[30px]">
+      <div className="flex h-[80%] w-[80%] flex-col items-center gap-[30px]">
         <div className="text-center">
-          <h1 className="text-[50px]">Harry Potter</h1>
+          <h1 className="text-[50px]">{call.callerInfo?.name}</h1>
           <h2>calling</h2>
         </div>
         <div className="h-[70%] w-[70%]">
-          <Image src="/avatars/1.png" alt="harry" fill className="!relative" />
+          {call.callerInfo && (
+            <Image
+              src={call.callerInfo.profilePicture as string}
+              alt="harry"
+              fill
+              className="!relative"
+            />
+          )}
         </div>
         <button
           className="flex h-16 w-16 items-center justify-center rounded-full bg-red-600"
