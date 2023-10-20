46861a22a9735f6dc5cc195623282b3506305efb
Sinkyo <yhko1988@gmail.com>
2023-10-20 15:25:02 +0900

    feat : 발신자 정보 표시

--- src/components/Calling/IncomingCall.tsx
+++ src/components/Calling/IncomingCall.tsx
@@ -3,21 +3,28 @@ import React, { useContext } from "react";
 import { SocketCotext } from "../common/CallingContext";
 
 const IncomingCall = () => {
-  const { rejectUser, answerUser } = useContext(SocketCotext);
+  const {
+    rejectUser,
+    answerUser,
+    call: { callerInfo },
+  } = useContext(SocketCotext);
+
   return (
     <div className="absolute bottom-3 left-1/2 z-[99] flex h-[120px] w-[400px] -translate-x-1/2 items-center justify-start border-2 border-teal-800 bg-panel-header-background p-3">
       <div className="flex items-center gap-[20px]">
         <div className="h-[80px] w-[80px] rounded-sm">
-          <Image
-            className="!relative"
-            src="/avatar.jpg"
-            alt="incoming-profile-picture"
-            fill
-          />
+          {callerInfo && (
+            <Image
+              className="!relative"
+              src={callerInfo.profilePicture as string}
+              alt="incoming-profile-picture"
+              fill
+            />
+          )}
         </div>
         <div className="flex flex-col gap-[10px]">
           <div>
-            <div className="text-[20px]">Harvy spector</div>
+            <div className="text-[20px]">{callerInfo && callerInfo.name}</div>
             <div>incoming call</div>
           </div>
           <div className="flex gap-[5px]">
