f5e0bee434ba68bff5b0e70f6f024c92b8cc69da
Sinkyo <yhko1988@gmail.com>
2023-11-02 21:40:02 +0900

    feat : 로딩 컴포넌트 완성

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,7 +1,9 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
 const Loading = () => {
   return (
-    <div className="flex h-full items-center justify-center">
-      Loading ㅋㅋㅋ
+    <div className="animate-spinning absolute -top-[45%] left-1/2 flex h-full items-center justify-center">
+      <AiOutlineLoading3Quarters className="h-[30px] w-[30px]" />
     </div>
   );
 };
