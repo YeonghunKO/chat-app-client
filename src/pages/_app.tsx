a6f2a9a5890c36c4cf4c578c1126675298338c71
Sinkyo <yhko1988@gmail.com>
2023-11-02 16:17:12 +0900

    refactor : 우선 suspense는 각자의 instance에서 개별처리하는 것으로

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,6 @@ export default function App({ Component, pageProps }: AppProps) {
         defaultOptions: {
           queries: {
             retry: 1,
-            // suspense: true,
             useErrorBoundary: true,
             refetchOnWindowFocus: false,
           },
