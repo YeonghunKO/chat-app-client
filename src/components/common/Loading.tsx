ce21172eae2eb241d2781f2aaf977edc4c5b7bac
Sinkyo <yhko1988@gmail.com>
2023-11-08 11:20:26 +0900

    style : 로딩바 위치 조정

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -2,7 +2,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Loading = () => {
   return (
-    <div className="animate-spinning absolute -top-[45%] left-1/2 flex h-full items-center justify-center">
+    <div className="absolute -top-[45%] left-1/2 z-[999] flex h-full animate-spinning items-center justify-center">
       <AiOutlineLoading3Quarters className="h-[30px] w-[30px]" />
     </div>
   );
