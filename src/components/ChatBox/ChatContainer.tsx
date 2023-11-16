6820bfc852ee37eb6a3ac164ff16f316e15cfe77
Sinkyo <yhko1988@gmail.com>
2023-11-16 10:43:28 +0900

    style : 로딩 컴포넌트로 인해 스크롤바가 생겼다 없어졌다 함
    - absolute로 띄워서 영향 안주게 함

--- src/components/ChatBox/ChatContainer.tsx
+++ src/components/ChatBox/ChatContainer.tsx
@@ -6,10 +6,10 @@ const ChatContainer = () => {
   return (
     <div className="custom-scrollbar flex h-[80dvh] w-full flex-grow flex-col-reverse overflow-auto ">
       <div className="fixed left-0 top-0 z-0 h-full w-full bg-chat-background bg-fixed opacity-5"></div>
-      <div className="relative mx-10 my-6 ">
+      <div className="relative mx-10 my-6 h-auto ">
         <Suspense
           fallback={
-            <div className="h-[60dvh]">
+            <div className="absolute -top-[40dvh] left-1/2">
               <Loading />
             </div>
           }
