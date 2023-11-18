68f31f440bfcd6f46fdf77ae0f9296d0179ba59f
Sinkyo <yhko1988@gmail.com>
2023-11-18 16:40:19 +0900

    style : 채팅방 가장자리 간격 조정

--- src/components/ChatBox/ChatContainer.tsx
+++ src/components/ChatBox/ChatContainer.tsx
@@ -5,8 +5,8 @@ import Loading from "../common/Loading";
 const ChatContainer = () => {
   return (
     <div className="custom-scrollbar flex h-[80dvh] w-full flex-grow flex-col-reverse overflow-auto ">
-      <div className="fixed left-0 top-0 z-0 h-full w-full bg-chat-background bg-fixed opacity-5"></div>
-      <div className="relative mx-10 my-6 h-auto ">
+      {/* <div className="fixed left-0 top-0 z-0 h-full w-full bg-chat-background bg-fixed opacity-5"></div> */}
+      <div className="relative m-[10px] h-auto md:m-[30px]">
         <Suspense
           fallback={
             <div className="absolute -top-[40dvh] left-1/2">
