de7311e48cf66d12229dd7831e08faebca9037b1
Sinkyo <yhko1988@gmail.com>
2023-11-20 17:24:32 +0900

    style : 반응형 완성

--- src/components/Calling/ConnectedCall.tsx
+++ src/components/Calling/ConnectedCall.tsx
@@ -6,34 +6,30 @@ const ConnectedCall = () => {
   const { myVideo, userVideo, cancelUser, call } = useContext(SocketCotext);
 
   return (
-    <div className="absolute left-1/2 top-1/2 z-[99] flex h-[80dvh] w-[80dvw] -translate-x-1/2 -translate-y-1/2 items-center justify-center border-2 border-outgoing-background  bg-conversation-panel-background">
-      <div className="flex h-[80%] w-[80%] flex-col items-center gap-[30px]">
+    <div className="absolute left-1/2 top-1/2 z-[99] flex h-[90dvh] w-[90dvw] -translate-x-1/2 -translate-y-1/2 items-center justify-center border-2 border-outgoing-background bg-conversation-panel-background py-[10px]">
+      <div className="flex h-full w-[90%] flex-col items-center justify-center gap-[30px] md:w-[80%] md:gap-[30px]">
         <div>
-          <h1 className="text-[30px]">
-            On the phone with {call.callerInfo?.name}
+          <h1 className="text-center text-[20px] md:text-[30px]">
+            On the phone with <br /> <b>{call.callerInfo?.name}</b>
           </h1>
         </div>
-        <div className="flex  h-full w-full items-center justify-center gap-[20px]">
-          <div className="h-full w-full">
-            <video
-              playsInline
-              ref={myVideo}
-              autoPlay
-              muted
-              className="h-full w-full border-2 border-zinc-700 object-fill"
-            />
-          </div>
-          <div className="h-full w-full">
-            <video
-              playsInline
-              ref={userVideo}
-              autoPlay
-              className="h-full w-full border-2 border-zinc-700 object-fill"
-            />
-          </div>
+        <div className="flex  flex-col items-center justify-center gap-[20px] md:flex-row">
+          <video
+            playsInline
+            ref={myVideo}
+            autoPlay
+            muted
+            className="h-[25dvh] w-[400px] border-2 border-zinc-700 object-fill md:h-[400px] md:w-[300px] lg:w-[40dvw]"
+          />
+          <video
+            playsInline
+            ref={userVideo}
+            autoPlay
+            className="h-[25dvh] w-[400px] border-2 border-zinc-700 object-fill md:h-[400px] md:w-[300px] lg:w-[40dvw]"
+          />
         </div>
         <button
-          className="flex h-16 w-16 items-center justify-center rounded-full bg-red-600"
+          className="flex h-[40px] w-[60px] items-center justify-center rounded-full bg-red-600"
           onClick={cancelUser}
         >
           <MdOutlineCallEnd className="text-3xl" />
