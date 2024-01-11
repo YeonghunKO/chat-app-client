b6523df395ef353423892a907b608ff3bc584593
Sinkyo <yhko1988@gmail.com>
2024-01-11 09:53:28 +0900

    fix : 불필요한 세팅 삭제

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost"],
+    domains: ["https://chat-app-client-yeonghunko.vercel.app"],
     remotePatterns: [
       {
         hostname: "**",
@@ -14,33 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
-  rewrites: () => [
-    {
-      source: "/:path*",
-      destination: "http://chat-appbackend.hopto.org:3005/:path*",
-    },
-  ],
 };
 
 module.exports = nextConfig;
