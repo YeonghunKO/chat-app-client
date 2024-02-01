a6a28436e53eeb593ca34afbb37d9bd9f40781c6
Sinkyo <yhko1988@gmail.com>
2024-02-01 15:21:57 +0900

    refactor : 이미지 사이즈랑  설명 글자 조정

--- src/constant/defaultSEO.ts
+++ src/constant/defaultSEO.ts
@@ -6,12 +6,12 @@ export const DEFAULT_SEO = {
     locale: "ko_KR",
     url: "https://chat-app.live/",
     title: "친구랑 신나는 채팅을 해보아요 💬",
-    description: "영상통화랑 메시지 필터링 등 여러가지 것들을 체험해보세요👀",
+    description: "채팅 외 여러가지 기능이 제공되요👀",
     images: [
       {
         url: "https://firebasestorage.googleapis.com/v0/b/whats-app-chat-84e62.appspot.com/o/fun_chat.webp?alt=media&token=ed830532-1469-4b6c-bc01-27834e82df08",
-        width: 285,
-        height: 167,
+        width: 1200,
+        height: 630,
         alt: "chatting_default_image",
       },
     ],
