15cc787b82e78a7f60d47528395703b68331ed4b
Sinkyo <yhko1988@gmail.com>
2024-02-15 11:56:49 +0900

    feat : 로그인 서버 핸들러 마킹

---
+++ __test__/__mocks__/constant.ts
@@ -0,0 +1,13 @@
+export const MOCKED_LOGIN_USER_INFO = {
+  email: "sinkyo@gmail.com",
+  password: "test123",
+  name: "sinkyo",
+  profilePicture: "sinkyo_profile.png",
+  about: "me",
+};
+
+export const MOCKED_USERS = [
+  "sinkyo@gmail.com",
+  "aeika@gmail.com",
+  "amaya@gmail.com",
+];
