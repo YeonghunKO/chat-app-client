be683bddba0260ac0e086c32e14684378bd965ea
Sinkyo <yhko1988@gmail.com>
2024-02-16 16:36:11 +0900

    feat : 로그인 성공케이스 테스트 코드 작성 완료
    - 서버 모킹도 함께 함

--- __test__/login.test.tsx
+++ __test__/login.test.tsx
@@ -1,19 +1,38 @@
-import "@testing-library/jest-dom";
 import { screen } from "@testing-library/react";
-import Login from "@/pages/Login";
-import { customRender } from "./customRender";
+import userEvent from "@testing-library/user-event";
 
+import { customRender } from "./customRender";
+import Login from "@/pages/login";
+import { MOCKED_LOGIN_USER_INFO } from "../src/mocks/constant";
+import { DASHBOARD } from "@/constant/path";
+
+const pushMock = jest.fn();
 jest.mock("next/navigation", () => ({
   useRouter() {
     return {
-      push: () => null,
+      push: pushMock,
     };
   },
 }));
 
 describe("Login", () => {
-  it("test login", () => {
+  it.only("successfully logged in", async () => {
     customRender(<Login />);
-    screen.debug();
+
+    const emailInput = screen.getByRole("textbox", { name: "Display Email" });
+    const passwordInput = screen.getByLabelText("Password");
+    const submitButton = screen.getByRole("button", {
+      name: "Login an Account",
+    });
+
+    await userEvent.type(emailInput, MOCKED_LOGIN_USER_INFO.email);
+    await userEvent.type(passwordInput, MOCKED_LOGIN_USER_INFO.password);
+    await userEvent.click(submitButton);
+    expect(pushMock).toHaveBeenCalledWith(DASHBOARD);
+  });
+
+  it("failed to log in", () => {
+    customRender(<Login />);
+    // screen.debug();
   });
 });
