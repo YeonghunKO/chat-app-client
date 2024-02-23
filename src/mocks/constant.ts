c616f04a8b4c87064244f70f1610bd1b111ce544
Sinkyo <yhko1988@gmail.com>
2024-02-23 14:36:05 +0900

    feat : dashboard api 모킹

--- src/mocks/constant.ts
+++ src/mocks/constant.ts
@@ -1,3 +1,5 @@
+import { IMessage } from "@/type";
+
 export const MOCKED_LOGIN_USER_INFO = {
   email: "sinkyo@gmail.com",
   password: "test123",
@@ -11,8 +13,129 @@ export const MOCKED_LOGGED_IN_USER_INFO = {
   name: "sinkyo",
   profilePicture: "sinkyo_profile.png",
   about: "me",
-  id: 1,
+  id: 3,
 };
+
+export const MOCKED_MESSAGES = [
+  [
+    "2023-12-07",
+    [
+      {
+        id: 693,
+        senderId: 3,
+        recieverId: 7,
+        message: "hey",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-07T01:32:41.076Z",
+      },
+      {
+        id: 694,
+        senderId: 3,
+        recieverId: 7,
+        message: "sub",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-07T01:58:39.459Z",
+      },
+      {
+        id: 695,
+        senderId: 3,
+        recieverId: 7,
+        message: "uploads/images/1701914517359apple.WEBP",
+        type: "image",
+        status: "read",
+        createdAt: "2023-12-07T02:01:57.361Z",
+      },
+      {
+        id: 696,
+        senderId: 3,
+        recieverId: 7,
+        message: "cool",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-07T02:03:03.022Z",
+      },
+      {
+        id: 697,
+        senderId: 7,
+        recieverId: 3,
+        message: "sub",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-07T02:03:44.847Z",
+      },
+      {
+        id: 698,
+        senderId: 3,
+        recieverId: 7,
+        message: "cool",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-07T02:03:51.682Z",
+      },
+      {
+        id: 699,
+        senderId: 3,
+        recieverId: 7,
+        message: "🌐😃ww",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-07T02:04:53.951Z",
+      },
+      {
+        id: 700,
+        senderId: 7,
+        recieverId: 3,
+        message: "d",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-07T02:06:01.609Z",
+      },
+      {
+        id: 701,
+        senderId: 3,
+        recieverId: 7,
+        message: "fw",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-07T02:06:22.117Z",
+      },
+    ],
+  ],
+  [
+    "2023-12-14",
+    [
+      {
+        id: 702,
+        senderId: 7,
+        recieverId: 3,
+        message: "안녕!",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-14T12:51:00.464Z",
+      },
+      {
+        id: 703,
+        senderId: 3,
+        recieverId: 7,
+        message: "하이",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-14T12:51:08.644Z",
+      },
+      {
+        id: 705,
+        senderId: 3,
+        recieverId: 7,
+        message: "어이",
+        type: "text",
+        status: "read",
+        createdAt: "2023-12-14T12:51:34.630Z",
+      },
+    ],
+  ],
+] satisfies [string, IMessage[]][];
 
 export const MOCKED_ONLINE_USERS = new Map();
 
--- src/mocks/handlers/dashboard.handler.ts
+++ src/mocks/handlers/dashboard.handler.ts
@@ -1,14 +1,24 @@
 import { DefaultBodyType, PathParams, rest } from "msw";
-import { MOCKED_LOGGED_IN_USER_INFO } from "../constant";
-import { GET_USER } from "@/constant/api";
+import {
+  MOCKED_CURRENT_CHAT_USER,
+  MOCKED_LOGGED_IN_USER_INFO,
+  MOCKED_MESSAGES,
+} from "../constant";
+import { GET_MESSAGES, GET_USER } from "@/constant/api";
 
 type GetLoggedInUserInfo = typeof MOCKED_LOGGED_IN_USER_INFO;
 
 export const dashboardHandlers = [
   rest.get<DefaultBodyType, PathParams, GetLoggedInUserInfo>(
-    GET_USER(MOCKED_LOGGED_IN_USER_INFO.about),
+    GET_USER(MOCKED_LOGGED_IN_USER_INFO.email),
     (req, res, ctx) => {
       return res(ctx.status(201), ctx.json(MOCKED_LOGGED_IN_USER_INFO));
+    },
+  ),
+  rest.get<DefaultBodyType, PathParams, typeof MOCKED_MESSAGES>(
+    GET_MESSAGES(MOCKED_LOGGED_IN_USER_INFO.id, MOCKED_CURRENT_CHAT_USER.id),
+    (req, res, ctx) => {
+      return res(ctx.status(201), ctx.json(MOCKED_MESSAGES));
     },
   ),
 ];
