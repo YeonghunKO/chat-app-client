37be7f49fc3adce9c6fd70adfdb7c24c450e3e1c
Sinkyo <yhko1988@gmail.com>
2024-02-26 17:09:49 +0900

    feat : message post api 모킹

--- src/mocks/handlers/dashboard.handler.ts
+++ src/mocks/handlers/dashboard.handler.ts
@@ -4,9 +4,15 @@ import {
   MOCKED_LOGGED_IN_USER_INFO,
   MOCKED_MESSAGES,
 } from "../constant";
-import { GET_MESSAGES, GET_USER } from "@/constant/api";
+import { ADD_MESSAGE, GET_MESSAGES, GET_USER } from "@/constant/api";
 
 type GetLoggedInUserInfo = typeof MOCKED_LOGGED_IN_USER_INFO;
+
+type PostSendMessage = {
+  from: number;
+  to: number;
+  message: string;
+};
 
 export const dashboardHandlers = [
   rest.get<DefaultBodyType, PathParams, GetLoggedInUserInfo>(
@@ -21,4 +27,30 @@ export const dashboardHandlers = [
       return res(ctx.status(201), ctx.json(MOCKED_MESSAGES));
     },
   ),
+  rest.post<
+    PostSendMessage,
+    PathParams,
+    {
+      id: number;
+      senderId: number;
+      recieverId: number;
+      message: string;
+      type: string;
+      status: string;
+      createdAt: string;
+    }
+  >(ADD_MESSAGE, (req, res, ctx) => {
+    return res(
+      ctx.status(201),
+      ctx.json({
+        id: 3,
+        senderId: MOCKED_LOGGED_IN_USER_INFO.id,
+        recieverId: MOCKED_CURRENT_CHAT_USER.id,
+        message: req.body.message,
+        type: "text",
+        status: "sent",
+        createdAt: new Date().toISOString(),
+      }),
+    );
+  }),
 ];
